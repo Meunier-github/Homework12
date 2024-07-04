9f8f8fb464cf26fd256e3cfc1d780ed1fa921509
meunier-github <meunier.natalia@gmail.com>
2024-07-04 09:41:08 +0200

    Homework13.

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private String titleBook;
     private Author author;
@@ -11,9 +13,11 @@ public class Book {
     public String getTitleBook() {
         return titleBook;
     }
+
     public Author getAuthor() {
         return author;
     }
+
     public int getYearOfPublication() {
         return yearOfPublication;
     }
@@ -21,4 +25,31 @@ public class Book {
     public void setYearOfPublication(int yearOfPublication) {
         this.yearOfPublication = yearOfPublication;
     }
+
+    public void setTitleBook(String titleBook) {
+        this.titleBook = titleBook;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return titleBook + ". " + author + ". Год издания " + yearOfPublication;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearOfPublication == book.yearOfPublication && Objects.equals(titleBook, book.titleBook) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(titleBook, author, yearOfPublication);
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -8,24 +8,15 @@ public class Main {
         Book voinaImir = new Book("Война и мир",tolstoi,2020);
 
 
-        System.out.println("kapitanskayDochka.getTitleBook() = " + kapitanskayDochka.getTitleBook());
-        System.out.println("kapitanskayDochka.getAuthor() = " +
-                    kapitanskayDochka.getAuthor().getNameAuthor() + " "+
-                    kapitanskayDochka.getAuthor().getSurnameAuthor());
-
-        System.out.println("kapitanskayDochka.getYearOfPublication() = " + kapitanskayDochka.getYearOfPublication());
-
-        System.out.println("voinaImir.getTitleBook() = " + voinaImir.getTitleBook());
-        System.out.println("voinaImir.getAuthor() = " +
-                        voinaImir.getAuthor().getNameAuthor()+" "+
-                        voinaImir.getAuthor().getSurnameAuthor()
-                );
-        System.out.println("voinaImir.getYearOfPublication() = " + voinaImir.getYearOfPublication());
+        System.out.println("kapitanskayDochka.toString() = " + kapitanskayDochka.toString());
+        System.out.println("voinaImir.toString() = " + voinaImir.toString());
 
         voinaImir.setYearOfPublication(2021);
 
-        System.out.println("voinaImir.getYearOfPublication() = " + voinaImir.getYearOfPublication());
+        System.out.println("voinaImir.toString() = " + voinaImir.toString());
+
 
 
     }
 }
+
